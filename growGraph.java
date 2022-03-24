69a9b9f0e5ca7f16b86249969e8ff9c6d273c184
Sivarama Krishnan Venkatrayan <53943398+sivaramakrishnan1@users.noreply.github.com>
2022-03-24 21:17:33 +0530

    Update growGraph.java
    The previous version had calculateCenter() which returns centroid's coordinate  (x and y value as an array). It is used to encrypt two characters of the message as a set with that coordinate. It turns out to be major problem when one of the coordinate is not changing after growth/shrink of the triangle, thus, creating limited skipped part of Caesar cipher encryption algorithm. So it is reverted to its original method of encrypting one character at a time.

--- growGraph.java
+++ growGraph.java
@@ -1,26 +1,22 @@
-//import java.util.Scanner;
-
 class growGraph {
-    static char[] alphabets={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', '0','1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    static char[] alphabets={' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', '0','1', '2', '3', '4', '5', '6', '7', '8', '9'};
     public static int[][] vertex = {{0, 0}, {0, 0}, {0, 0}};
     static int x = 0, y = 1, z = 2;
     static int counter = 0;
-
     public static boolean increaseTriangle;
-
+    
     public static void main(String args[]) {
-        //Scanner s = new Scanner( System.in );
         setVertex();
         System.out.print("\nEnter the text to be encrypted: ");
-        String plainText = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String plainText = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789";
         System.out.print("\nPlain Text:\t" + plainText);
         String encodedText = encryptText(plainText.toCharArray());
         System.out.print("\nEncrypted Text:\t" + encodedText);
         String decodedText = decryptText(encodedText.toCharArray());
         System.out.print("\nDecrypted Text:\t" + decodedText + "\n");
-        //s.close(); 
     }
 
+    // returns the passed character's position in char[] alphabets.
     public static int findCharacter(char findable){
         for(int i=0 ; i<alphabets.length ; i++){
             if(alphabets[i] == findable)
@@ -28,41 +24,45 @@ class growGraph {
         }
         return -1;
     }
-
+    
+    // encryption magic happens here !!!
     public static String encryptText(char[] message) {
-        int[] key = { 0,0 };
-        for (int i = 0; i < message.length; i += 2) {
-            key = calculateCenter();
+        int key = 0;
+        for (int i = 0; i < message.length; i++) {
+            key = (calculateCenter()) % 63;
             int numChar = findCharacter(message[i]);
-            message[i] = alphabets[(((numChar + (key[0] % 62)) + 62) % 62)];
-            message[i] = alphabets[(((numChar + (key[1] % 62)) + 62) % 62)];
+            message[i] = alphabets[((numChar + key) % 63)];
         }
         return String.valueOf(message);
     }
-
+    
+    //decryption sorcery happen here !!!
     public static String decryptText(char[] message) {
-        int[] key = { 0,0 };
+        int key = 0;
         setVertex();
-        for (int i = 0; i < message.length; i += 2) {
-            key = calculateCenter();
+        for (int i = 0; i < message.length; i++) {
+            key = (calculateCenter()) % 63;
             int numChar = findCharacter(message[i]);
-            message[i] = alphabets[(((numChar - (key[0] % 62)) + 62) % 62)];
-            message[i] = alphabets[(((numChar - (key[1] % 62)) + 62) % 62)];
+            message[i] = alphabets[(((numChar - key) + 63) % 63)];
         }
         return String.valueOf(message);
     }
 
+    /*
+    // used to print vertex for debugging purpose.
     public static void printVertex() {
         System.out.print("\nVertex:");
         for (int i = 0; i < 3; i++) {
             System.out.print(vertex[i][0] + "," + vertex[i][1] + " ");
         }
     }
+    */
 
+    // initial process on initializing the vertex values.
     public static void setVertex() {
-        int[] publicKey = { 80,100 };
-        int[] privateKey1 = { 30,50 };
-        int[] privateKey2 = { 50,80 };
+        int[] publicKey = { 101,103 };
+        int[] privateKey1 = { 107,109 };
+        int[] privateKey2 = { 113,127 };
 
         increaseTriangle = false;
         counter = 0;
@@ -72,6 +72,7 @@ class growGraph {
         vertex[2] = privateKey2;
     }
 
+    // this function finds new vertex coordinates using the existing vertex coordinates as midpoints of sides of the triangle.
     public static void midpointsToVertex() {
         int[] currentX = {0, 0}, currentY = {0, 0}, currentZ = {0, 0};
 
@@ -85,7 +86,8 @@ class growGraph {
         vertex[1] = currentY;
         vertex[2] = currentZ;
     }
-
+    
+    // this function finds new vertex coordinates using the midpoints of sides of the triangle by existing vertex coordinates.
     public static void vertexToMidpoints() {
         int[] currentX = {0, 0}, currentY = {0, 0}, currentZ = {0, 0};
         
@@ -100,7 +102,8 @@ class growGraph {
         vertex[2] = currentZ;
     }
 
-    public static int[] calculateCenter() {
+    // This calculates the centroid of the triangle and also maintains the growth and shrink of the triangle
+    public static int calculateCenter() {
         int[] centroid = {0, 0};
         if(counter%5 == 0){
             increaseTriangle = !increaseTriangle;
@@ -109,16 +112,13 @@ class growGraph {
         counter++;
 
         if(increaseTriangle)
-            vertexToMidpoints();
-        else
             midpointsToVertex();
-
-        //printVertex();
-
+        else
+            vertexToMidpoints();
+        
         centroid[0] = (vertex[0][0] + vertex[1][0] + vertex[2][0]) / 3;
         centroid[1] = (vertex[0][1] + vertex[1][1] + vertex[2][1]) / 3;
-        //System.out.println("Centroid: " + centroid[0] + " " + centroid[1] + " ");
-        int center[] = {centroid[0] , centroid[1]};
+        int center = centroid[0] + centroid[1];
         return center;
     }
 }
